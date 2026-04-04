9ca84985d4d99f3722769b6125ee598380df2404
Erers1 <kd205174@gmail.com>
2026-04-04 08:21:48 +0700

    Modified 2

--- src/main/java/gr5/ecomerce/entity/AttributeValue.java
+++ src/main/java/gr5/ecomerce/entity/AttributeValue.java
@@ -6,7 +6,7 @@ import lombok.*;
 import java.util.List;
 
 @Entity
-@Table(name = "tblAttrVal")
+@Table(name = "tblAttrValue")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
--- src/main/java/gr5/ecomerce/entity/ProductAttrValue.java
+++ src/main/java/gr5/ecomerce/entity/ProductAttrValue.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "tblAttrValue")
+@Table(name = "tblProductAttrValue")
 @Getter
 @Setter
 @AllArgsConstructor
