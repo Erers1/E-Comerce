91fac69ae03389a3ff168e093eaeb45f19e96cdc
Erers1 <kd205174@gmail.com>
2026-04-08 17:11:19 +0700

    adding comment func 6

--- src/main/java/gr5/ecomerce/controller/CommentController.java
+++ src/main/java/gr5/ecomerce/controller/CommentController.java
@@ -15,7 +15,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//
 @RestController
 @RequestMapping("/api/product/comment")
 @RequiredArgsConstructor
